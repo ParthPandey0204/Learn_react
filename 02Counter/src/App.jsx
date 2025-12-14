a5c682f10fad34cc177996caf36f7a3229abd71c
ParthPandey0204 <parth.pandey80045@gmail.com>
2025-12-14 13:08:50 +0530

    updated code for counter

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
 
 function App() {
-   var value = 7
+   let [value , setValue] = useState(7)
   const addValue = ()=>{
-    value = value +1
-  }
+    setValue(prev => prev+1) 
+   }
   const subtractValue = ()=>{
-    value = value-1
+    if(value >= 1){
+    setValue(prev => prev-1)
+    }
   }
 
   return (
     <>
     <h1>This is just for fun</h1>
     <h2>Counter: {value}</h2>
-
     <button 
     onClick={addValue}
     >Add Value</button><br/>
